8777ea886b5c6e95974e1bcabd903f090b7ce89d
AnastasiiaLazar <anastacialazar21@gmail.com>
2022-11-20 20:29:27 +0200

    02.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,14 @@
 import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', setWatchingTime);
+player.on('timeupdate', throttle(setWatchingTime, 1000));
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 function setWatchingTime(e) {
     localStorage.setItem('videoplayer-current-time', e.seconds)    
-}
-
+};
