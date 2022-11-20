eac958d5af4e183af2edb6cffaa362a2700bc95e
AnastasiiaLazar <anastacialazar21@gmail.com>
2022-11-20 16:41:48 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const refs = {
+    galleryContainer: document.querySelector(".gallery"),
+}
+
+const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) =>
+        `<a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}" 
+                alt="${description}"
+            />
+        </a>`)
+    .join("");
+
+
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
